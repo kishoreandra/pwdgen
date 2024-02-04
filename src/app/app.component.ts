84d826483c18470a2bd6fb247282653aa8a4ede5
Kishore Kumar Andra <50901558+kishoreandra@users.noreply.github.com>
2024-02-04 15:25:13 +0530

    pwd gen - initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,33 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'pwdgen';
+  length = 0;
+  password = '';
+  hasNums = false;
+  hasLetters = false;
+  hasSymbols = false;
+
+  onChangeAlphabets() {
+    this.hasLetters = !this.hasLetters;
+  }
+  onChangeNums() {
+    this.hasNums = !this.hasNums;
+  }
+  onChangeSymbols() {
+    this.hasSymbols = !this.hasSymbols;
+  }
+  onChangeLength(eventTarget: EventTarget): void {
+    const value: string = (eventTarget as HTMLInputElement).value;
+    const length = Number(value);
+    if (Number.isInteger(length)) {
+      this.length = length;
+    }
+  }
+
+  onButtonClick() {
+    this.password = 'My passwordd !!!';
+  }
 }
